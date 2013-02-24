e6475ab391e1914e59756946eab1d5e3ae4d0097
Zuzana Dostalova <sanna23@icloud.com>
2013-02-24 21:37:17 +0100

    Ijacek exchange schedule

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,30 @@ class Person < ActiveRecord::Base
   attr_accessible :gender, :info, :password_hash, :password_salt, :username
   has_many :entries
 
+  def shedule
+    ["Tom", "Joc", "Arnie"]
+  end
+
+  def current
+    Person.where(:ijacek => true).first
+  end
+
+  def next
+    # zjistim na jake pozici v poli je current
+    position = schedule.index(current)
+    # current nastavim na false
+    current.ijacek = false
+    current.save
+    # ten dalsi po current se nastavi na true
+    new_position = position + 1
+    name = schedule[new_position]
+    next_person = Person.find_by_username("name")
+    next_person.ijacek = true
+    next_person.save
+    # vratim toho dalsiho
+    return next_person
+  end
+
   def image_or_default
     image || "default.png"
   end
