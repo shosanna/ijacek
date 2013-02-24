7140589b9375f74aeed6216a09171a8ba3a998bc
Zuzana Dostalova <sanna23@icloud.com>
2013-02-24 12:48:47 +0100

    Use default image if missing

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,9 @@ class Person < ActiveRecord::Base
   has_secure_password
   attr_accessible :gender, :info, :password_hash, :password_salt, :username
   has_many :entries
+
+  def image_or_default
+    image || "default.png"
+  end
+
 end
