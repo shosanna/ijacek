e0f55332ff30ea3129f0f2f88b09e1e6ba79b406
Zuzana Dostalova <sanna23@icloud.com>
2013-03-07 22:50:36 +0100

    Answers duplication eliminated

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,25 @@
 class AnswersController < ApplicationController
   def create
-    @answer = Answer.new(params[:answer])
-    @answer.person = current_user
-    @answer.save!
-    flash.notice = "Answer created!"
-    redirect_to current_user
+
+    # Jestlize ma current user odpoved k dane otazce,
+    # najdu odpoved a dam update attributes na text.
+    # 1) musim zjistit jestli existuje answer, co ma person_id a
+    #  question_id co miri ke stejny veci, co jsou propojeny
+    # 2) Vythahnu answers od daneho cloveka a podivam se
+
+    answer = current_user.answers.where(:question_id => params[:answer][:question_id]).first
+
+    if answer
+      answer.update_attributes(params[:answer])
+      flash.notice = "Answer changed!"
+      redirect_to current_user
+    else
+      @answer = Answer.new(params[:answer])
+      @answer.person = current_user
+      @answer.save!
+      flash.notice = "Answer created!"
+      redirect_to current_user
+    end
   end
 
   def index
